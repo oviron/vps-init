7aeb5a7be232a76c4b0c7d8a864448fe3ed6b07f
Dmitry Avdonkin <awdonkin@gmail.com>
2022-03-06 17:24:14 +0300

    fix

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 adduser oviron
 usermod -a -G sudo oviron
-apt install fail2ban
+apt install fail2ban -y
 mv -f /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 mv -f ./sshd_config /etc/ssh/sshd_config
 mv -f ./user.rules /etc/ufw/user.rules
