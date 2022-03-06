e0ef94da0a61c9298ed8c0b83dd6edeabdb8ffd3
Dmitry Avdonkin <awdonkin@gmail.com>
2022-03-06 17:16:41 +0300

    fix

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-useradd oviron
+adduser oviron
 usermod -a -G sudo oviron
-apt install fail2ban
+apt install fail2ban -y
 mv -f /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 mv -f sshd_config /etc/ssh/sshd_config
-mv -f user_rules /etc/ufw/user.rules
+mv -f user.rules /etc/ufw/user.rules
 ufw disable
 ufw enable
 service ssh restart
