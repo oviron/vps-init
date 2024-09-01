65d840536682bfc6d6d4d410c5e3353583976e9c
Dmitry Avdonkin <awdonkin@gmail.com>
2024-09-01 23:41:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,8 @@ apt install fail2ban -y
 mv -f /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 mv -f ./sshd_config /etc/ssh/sshd_config
 mv -f ./user.rules /etc/ufw/user.rules
+mv -f ./user6.rules /etc/ufw/user6.rules
 ufw disable
 ufw enable
+ufw reload
 service ssh restart
