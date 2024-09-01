ba893e81d193738632c1efce4afe734161bfa64c
Dmitry Avdonkin <awdonkin@gmail.com>
2024-09-01 23:45:13 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ mv -f /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 mv -f ./sshd_config /etc/ssh/sshd_config
 mv -f ./user.rules /etc/ufw/user.rules
 mv -f ./user6.rules /etc/ufw/user6.rules
+mv -f ./sysctl.conf /etc/ufw/sysctl.conf
 ufw disable
 ufw enable
 ufw reload
